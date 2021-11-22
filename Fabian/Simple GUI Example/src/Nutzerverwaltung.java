222e3b0d735b46ece4d8537fb1593a5496948854
nmersmann <89512208+nmersmann@users.noreply.github.com>
2021-11-22 10:39:37 +0100

    Delete Fabian/Simple GUI Example directory

--- Fabian/Simple GUI Example/src/Nutzerverwaltung.java
+++
@@ -1,7 +0,0 @@
-import control.Controller;
-
-public class Nutzerverwaltung {
-    public static void main(String[] args) throws Exception {
-        new Controller();
-    }
-}
--- Fabian/Simple GUI Example/src/model/Teamleiter.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Teamleiter extends Person {
-
-    public Teamleiter(String vorname, String nachname, String teamName, int alter) {
-        super(vorname, nachname, teamName, alter);
-    }
-}
